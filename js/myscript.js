4f080299ac0b1c0df3f3d0223fa3deea38045cf4
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-21 17:22:49 +0100

    fixed decimals numbers of price

--- js/myscript.js
+++ js/myscript.js
@@ -7,24 +7,24 @@ const age = parseInt (prompt ("Quanti anni hai?"));
 console.log(age);
 
 // prezzo intero 
-const price = distance * 0.21  + "&euro;";
+const price = distance * 0.21;
 
 // sconto in base all'età 
 const discountOver = price * 0.40;
 const discountJunior = price * 0.20;
 
 // prezzo scontato 
-const discountPriceOver = price - discountOver + "&euro;";
-const discountPriceJunior = price - discountJunior + "&euro;";
+const discountPriceOver = price - discountOver;
+const discountPriceJunior = price - discountJunior;
 
 
 // calcolo del prezzo finale al cliente in base al'età 
 
 if (age < 18) {
-    document.getElementById ("final-price").innerHTML = discountPriceJunior;
+    document.getElementById ("final-price").innerHTML = discountPriceJunior.toFixed(2) + "&euro;";
 } else if (age > 65) {
-    document.getElementById ("final-price").innerHTML = discountPriceJunior;
+    document.getElementById ("final-price").innerHTML = discountPriceOver.toFixed(2) + "&euro;";
 }
 else {
-    document.getElementById ("final-price").innerHTML = price;
+    document.getElementById ("final-price").innerHTML = price + "&euro;";
 }
